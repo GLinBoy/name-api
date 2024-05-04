595db811d24066d1f111ce9cfa08ad9fc900f5be
GLinBoy <glinboy@live.com>
2024-05-04 16:45:52 +0200

    🕯 START: Initiated the project
    resolved #1

---
+++ src/test/kotlin/com/glinboy/name/NameApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.glinboy.name
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NameApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
