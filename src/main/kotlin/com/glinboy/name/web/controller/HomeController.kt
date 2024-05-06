48a210ab8f1c20dca06c93f17e138fd23c2f1475
GLinBoy <glinboy@live.com>
2024-05-06 17:34:58 +0200

    📦 NEW: Added Home controller to manage root requests
    #14

---
+++ src/main/kotlin/com/glinboy/name/web/controller/HomeController.kt
@@ -0,0 +1,8 @@
+package com.glinboy.name.web.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class HomeController {
+}
