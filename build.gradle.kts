3cd27ecc83f44fe7520cf3902427fe247d2c63ae
GLinBoy <glinboy@live.com>
2026-03-07 16:39:45 +0100

    👌 IMPROVE: Upgrade dependencies
    resolved #61

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 	id("io.spring.dependency-management") version "1.1.7"
 	kotlin("jvm") version "2.2.21"
 	kotlin("plugin.spring") version "2.2.21"
-	id ("com.github.ben-manes.versions") version "0.51.0"
+	id ("com.github.ben-manes.versions") version "0.53.0"
 	id ("org.ec4j.editorconfig") version "0.1.0"
 }
 
@@ -14,7 +14,7 @@ version = "0.0.1-SNAPSHOT"
 
 defaultTasks("bootRun")
 
-val springDocVersion = "2.6.0"
+val springDocVersion = "3.0.2"
 
 java {
 	toolchain {
